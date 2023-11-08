d972d1cd7ebf7e5b0494481e708ec73f220a8a2b
liyunlong <anlitylee@qq.com>
2023-11-08 14:29:15 +0800

    feat: 完善docker的Dockerfile文件

--- docker/server/docker-run.sh
+++ docker/server/docker-run.sh
@@ -2,7 +2,6 @@
 
 /chatait/backendServer/chatait-backend-server --gf.gcfg.path=/chatait/config &
 /chatait/frontendServer/chatait-frontend-server --gf.gcfg.path=/chatait/config &
-nginx -g "daemon off;"
 
 while [[ true ]]; do
     sleep 1
