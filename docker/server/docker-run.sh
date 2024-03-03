325298e12e1f8e0e6f95b5432a14caaded317a6a
leo <anlitylee@qq.com>
2024-03-03 18:59:50 +0800

    fix: 修复docker服务端构建

--- docker/server/docker-run.sh
+++ docker/server/docker-run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 /chatait/backendServer/chatait-backend-server --gf.gcfg.path=/chatait/config &
 /chatait/frontendServer/chatait-frontend-server --gf.gcfg.path=/chatait/config &
